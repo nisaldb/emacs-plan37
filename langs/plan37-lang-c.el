f4326efa1f7958f2b9feb19f5778754e1ef055fa
Nisal Bandara <nisalb@hey.com>
2024-06-02 18:50:35 +0530

    Add C-specific configurations

--- langs/plan37-lang-c.el
+++ langs/plan37-lang-c.el
@@ -24,7 +24,50 @@
 
 ;;; Code:
 
+(use-package cc-mode
+  :ensure nil
+  :config
+  ;; A new c style for PostgreSQL source files
+  (c-add-style "postgresql"
+               '("bsd"
+                 (c-auto-align-backslashes . nil)
+                 (c-basic-offset . 4)
+                 (c-offsets-alist . ((case-label . +)
+                                     (label . -)
+                                     (statement-case-open . +)))
+                 (fill-column . 78)
+                 (indent-tabs-mode . t)
+                 (tab-width . 4)))
 
+  (defvar plan37--use-postgresql-style-regex "/postgre\\(ql\\)?/"
+    "In C-mode, if the opened `buffer-file-name' matches this string, its
+C style is set to `postgresql' style.")
+  (defun plan37--use-postgresql-style-if-possible ()
+    "Enable `postgresql' style if applicable to the opened buffer."
+    (when (string-match plan37--use-postgresql-style-regex buffer-file-name)
+      (c-set-style "postgresql")
+      ;; This might not be required. But kept for portability.
+      (set (make-local-variable 'ignored-local-variables)
+           (append '(c-file-style) ignored-local-variables))))
+  (add-hook 'c-mode-hook #'plan37--use-postgresql-style-if-possible)
+
+  ;; default C styles
+  (setq c-default-style '((java-mode . "java") (awk-mode . "awk") (other . "bsd")))
+  (setq c-basic-offset 4)
+
+  ;; documentation style
+  (setq c-doc-comment-style '((java-mode . javadoc) (c-mode . doxygen)))
+
+  ;; make RET to indent the new line
+  (defun plan37--make-CR-do-indent ()
+    (define-key c-mode-base-map (kbd "<ret>") 'c-context-line-break))
+  (add-hook 'c-initialization-hook #'plan37--make-CR-do-indent))
+
+(use-package meson-mode
+  :ensure t
+  :hook (c-mode . meson-mode)
+  :init
+  (setq meson-indent-basic 4))
 
 (provide 'plan37-lang-c)
 ;;; plan37-lang-c.el ends here
